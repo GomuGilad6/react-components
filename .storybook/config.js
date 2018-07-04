7776aa4740aea2dffca0216f76cc115893cde891
unknown <giladbar6@gmail.com>
2018-07-04 23:30:02 +0300

    fix story file name

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure, addDecorator } from "@storybook/react";
 import Container from "./Container";
 
 // automatically import all files ending in *.story.js
-const req = require.context("../src", true, /.story.js$/);
+const req = require.context("../src", true, /.stories.js$/);
 
 function loadStories() {
     req.keys().forEach(req);
